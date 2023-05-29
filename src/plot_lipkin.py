613f361733c41c54221e14c6f488793f71d3f52c
hkve <haakon.kvernmoen@gmail.com>
2023-05-29 19:59:25 +0200

    La til heatmaps for HF og RPA

--- src/plot_lipkin.py
+++ src/plot_lipkin.py
@@ -3,6 +3,7 @@ import lipkin_utils
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 from qiskit import Aer
 from qiskit.circuit.library import RealAmplitudes
@@ -34,25 +35,25 @@ def run_lipkin_DIAG(v, w, N=4):
     return E[0]
 
 def run_HF(v, w, N=4):
-    v_tilde = v*(N-1)
+    R = (v+w)*(N-1)
     
     cond = 1
     front = -N/2
-    if v_tilde > 1:
-        cond = (1+(N-1)**2*v**2) / (2*(N-1)*v)
+    if R > 1:
+        cond = (1+(N-1)**2*(v+w)**2) / (2*(N-1)*(v+w))
 
-    return front*cond
+    return front*cond + w
 
 def run_RPA(v, w, N=4):
-    v_tilde = v*(N-1)
+    R = (v+w)*(N-1)
 
-    A, B = 1,-(N-1)*v
+    A, B = 1-(N-1)*w,-(N-1)*v
 
-    if v_tilde > 1:
-        A = 3*(N-1)**2 * v / (2*(N-1)*v - 1)
-        B = - (N-1)**2 * v**2 / (2*(N-1)*v+1)
+    if R > 1:
+        A = (3*(N-1)**2 * (v+w)**2 - 1) / (2*(N-1)*(v+w)) - (N-1)*w
+        B = - (1 + (N-1)**2 * (v+w)**2 )/ (2*(N-1)*(v+w)) + (N-1)*w
     
-    omega = np.sqrt(A**2 - B**2)
+    omega = np.sqrt(A**2 - np.abs(B)**2)
     return run_HF(v, w, N) + (omega - A)/2
 
 def plot_v_vs_E(N=4):
@@ -81,45 +82,56 @@ def plot_v_vs_E(N=4):
     plot_utils.save(f"N{N}_lipkin_vary_V")
     plt.show()
 
-def plot_diff_heatmap(N=4, load=False, filename="test"):
+def plot_diff_heatmap(N=4, load=False, filename="test", method="VQE", vocal=False):
     n_pts = 50
     v_range = np.linspace(0,2, n_pts)
     w_range = np.linspace(0,2, n_pts)
 
+    run_lipkin_get_E = {
+        "HF": run_HF,
+        "RPA":run_RPA,
+        "VQE": run_lipkin_VQE,
+    }
+
+    assert method in list(run_lipkin_get_E.keys()), f"{method = } is not valid. Must be one of {run_lipkin_get_E.keys()}"
+
     v_grid, w_grid = np.meshgrid(v_range, w_range, indexing="ij")
-    E_exact, E_vqe = np.zeros_like(v_grid), np.zeros_like(v_grid)
+    E_exact, E_calc = np.zeros_like(v_grid), np.zeros_like(v_grid)
 
     if not load:
         for i in range(n_pts):
             for j in range(n_pts):
                 v, w = v_grid[i, j], w_grid[i,j]
                 E_exact[i,j] = run_lipkin_DIAG(v, w, N=N)
-                E_vqe[i,j] = run_lipkin_VQE(v, w, N=N)
-                print(f"done {(j+1) + i*n_pts}, {v = }, {w = }")
+                E_calc[i,j] = run_lipkin_get_E[method](v, w, N=N)
+                if vocal:
+                    print(f"done {(j+1) + i*n_pts}, {v = }, {w = }")
             
         with open(f"{filename}.npy", "wb") as file:
             np.save(file, v_grid)
             np.save(file, w_grid)
             np.save(file, E_exact)
-            np.save(file, E_vqe)
+            np.save(file, E_calc)
 
     if load:
         with open(f"{filename}.npy", "rb") as file:
             v_grid = np.load(file)
             w_grid = np.load(file)
             E_exact = np.load(file)
-            E_vqe = np.load(file)
+            E_calc = np.load(file)
             
+    diff = np.abs((E_exact - E_calc)/E_exact) * 100
 
-    diff = np.abs((E_exact - E_vqe)/E_exact) * 100
     fig, ax = plt.subplots()
     cs = ax.contourf(v_grid, w_grid, diff)
     cbar = fig.colorbar(cs, ax=ax)
-    cbar.set_label(r"VQE \% relative error", rotation=90)
+    cbar.set_label(rf"{method} \% relative error", rotation=90)
     ax.set(xlabel=r"$V/\epsilon$", ylabel=r"$W/\epsilon$")
     plot_utils.save(filename)
     plt.show()
 if __name__ == '__main__':
     # plot_v_vs_E(N=2)
     # plot_v_vs_E(N=4)
-    plot_diff_heatmap(load=True, filename="50pts_vw_grid")
+    # plot_diff_heatmap(load=False, filename="50pts_vw_grid", vocal=True)
+    plot_diff_heatmap(method="HF", filename="50pts_vw_grid_HF")
+    plot_diff_heatmap(method="RPA", filename="50pts_vw_grid_RPA")
